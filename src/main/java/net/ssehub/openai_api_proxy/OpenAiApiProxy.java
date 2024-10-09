e408ebef24e969dd72a99791a30d2c356f63dd07
Adam Krafczyk <krafczyk@uni-hildesheim.de>
2024-10-09 14:12:22 +0200

    Remove unnecessary @Configuration

--- src/main/java/net/ssehub/openai_api_proxy/OpenAiApiProxy.java
+++ src/main/java/net/ssehub/openai_api_proxy/OpenAiApiProxy.java
@@ -3,13 +3,11 @@ package net.ssehub.openai_api_proxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import com.knuddels.jtokkit.Encodings;
 import com.knuddels.jtokkit.api.EncodingRegistry;
 
 @SpringBootApplication
-@Configuration
 public class OpenAiApiProxy {
     
     public static void main(String[] args) {
